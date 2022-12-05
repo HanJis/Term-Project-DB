935725e804d72a113e2156155aa82c2cbb82de4b
HanJis <74577822+HanJis@users.noreply.github.com>
2022-12-05 20:20:42 +0900

    도서 추가

--- JDBC_Test/src/Test.java
+++ JDBC_Test/src/Test.java
@@ -5,7 +5,7 @@ import javax.imageio.plugins.tiff.ExifGPSTagSet;
 import javax.print.attribute.standard.Finishings;
 
 public class Test {
-	
+
 	public static void main(String args[]) {
 
 		Scanner scanner = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class Test {
 			 * rs.getString(3)); con.close();
 			 */
 		} catch (Exception e) {
-			
+
 			System.out.println(e);
 			System.exit(0);
 		}
@@ -40,7 +40,7 @@ public class Test {
 				Book();
 
 			} else if (mainnum == 3) {
-				
+				scanner.close();
 				System.exit(0);
 			} else {
 				System.out.println("1~3의 정수를 입력해주세요.(현재 입력한 숫자 : " + mainnum + " )");
@@ -67,10 +67,11 @@ public class Test {
 			} else if (Custmnum == 2) {
 
 			} else if (Custmnum == 3) {
-				
+
 			} else if (Custmnum == 4) {
 
 			} else if (Custmnum == 5) {
+				scan.close();
 				break;
 			} else {
 				System.out.println("1~5의 정수를 입력해주세요.(현재 입력한 숫자 : " + Custmnum + " )");
@@ -93,24 +94,77 @@ public class Test {
 			System.out.println("---------------------------------------------");
 
 			booknum = scan.nextInt();
+			scan.nextLine();
 			if (booknum == 1) {
-
+				System.out.println("---------------------------------------------");
+				System.out.println("책의 이름을 입력해주세요");
+				String bname = scan.nextLine();
+				System.out.println("책의 출판사를 입력해주세요");
+				String publisher = scan.nextLine();
+				System.out.println("책의 가격을 입력해주세요");
+				Integer bprice = scan.nextInt();
+				Bookinsert(bname, publisher, bprice);
+				System.out.println("---------------------------------------------");
 			} else if (booknum == 2) {
 
 			} else if (booknum == 3) {
-				
+
 			} else if (booknum == 4) {
+				Booklook();
+			} else if (booknum == 5) {
 
-			}else if (booknum == 5) {
-
-			}  
-			else if (booknum == 6) {
+			} else if (booknum == 6) {
+				scan.close();
 				break;
 			} else {
 				System.out.println("1~5의 정수를 입력해주세요.(현재 입력한 숫자 : " + booknum + " )");
 			}
 		} while (booknum != 5);
 
+	}
+
+	public static void Bookinsert(String bname, String publisher, Integer bprice) {
+		int insertCount = 0;
+		PreparedStatement ps = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://192.168.56.101:3308/madang", "jshan",
+					"wltjd7851");
+			Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs =stmt.executeQuery("select * from Book");
+			rs.last();
+			int rowCount = rs.getRow();
+			rs.beforeFirst();
+			String sql = "INSERT INTO Book(bookid, bookname, publisher, price) VALUES (?,?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, rowCount+1);
+			ps.setString(2, bname.toString());
+			ps.setString(3, publisher.toString());
+			ps.setInt(4, bprice);
+			insertCount = ps.executeUpdate();
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+			System.exit(0);
+		} 
+	}
+
+	public static void Booklook() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://192.168.56.101:3308/madang", "jshan",
+					"wltjd7851");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Book");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+
+			System.out.println(e);
+			System.exit(0);
+		}
 	}
 
 }
